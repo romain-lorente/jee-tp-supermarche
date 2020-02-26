f0b7dd54ad09faefac3a901d217d6869b6c767f4
Opineppes <43174074+Opineppes@users.noreply.github.com>
2020-02-26 10:57:03 +0100

    add(Article) : Creation de la classe Article

---
+++ src/Article.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Article {
+    public static final int[] TVA = new int[] {550, 2000};
+
+    protected long codeBarre;
+    protected String reference;
+    protected String libelle;
+    protected int prixHT;
+    protected int tauxTVA;
+
+    public Article(long codeBarre, String reference, String libelle, int prixHT, int tauxTVA) {
+        this.codeBarre = codeBarre;
+        this.reference = reference;
+        this.libelle = libelle;
+        this.prixHT = prixHT;
+        this.tauxTVA = tauxTVA;
+    }
+
+    public long getCodeBarre() {
+        return codeBarre;
+    }
+
+    public void setCodeBarre(long codeBarre) {
+        this.codeBarre = codeBarre;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getPrixHT() {
+        return prixHT;
+    }
+
+    public void setPrixHT(int prixHT) {
+        this.prixHT = prixHT;
+    }
+
+    public int getTauxTVA() {
+        return tauxTVA;
+    }
+
+    public void setTauxTVA(int tauxTVA) {
+        this.tauxTVA = tauxTVA;
+    }
+}
