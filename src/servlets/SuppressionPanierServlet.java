fe5bef10728fa08f6de71a55c924979d7143191d
Opineppes <43174074+Opineppes@users.noreply.github.com>
2020-02-27 08:19:07 +0100

    Correction commit missing files: Ajout de la suppresion et de l'ajout d'article dans le panier

---
+++ src/servlets/SuppressionPanierServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import models.Article;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet("/suppressionPanier")
+public class SuppressionPanierServlet extends AbstractSuppressionServlet {
+
+    @Override
+    protected void validatedCase(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long[] selected = getSelected(request, response);
+
+        if(selected != null) {
+
+            if(selected.length > 0) {
+                HttpSession session = request.getSession();
+                Map<Long, Article> articles = (Map<Long, Article>) session.getAttribute("articles");
+
+                for(Long valeur : selected) {
+                    articles.remove(valeur);
+                }
+            }
+
+            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/suppressionPanierOk.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+
+    @Override
+    protected void toValidateCase(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long[] selected = getSelected(request, response);
+
+        if(selected != null) {
+            request.setAttribute("selected", selected);
+
+            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/suppressionPanier.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+}
